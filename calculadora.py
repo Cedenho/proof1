ed930fc8e61a19fc3328f1d554b55acfdf7c78a3
Cristian Cedeño <alejandrocabrera9909@gmail.com>
2023-03-25 14:12:12 -0500

    mi aporte

--- calculadora.py
+++ calculadora.py
@@ -1,12 +1,19 @@
-num1 = 5
-num2 = 6
+num1 = 10
+num2 = 8
 
 def suma(numero1, numero2):
     total = numero1 + numero2
     return total
 
+def resta(numero1, numero2):
+    total = numero1 - numero2
+    return total
+
+
 
 resultado = suma(num1, num2)
-print('el resultado es ' ,resultado)
+resultado2 = resta(num1,num2)
+print('El valor de la suma es ' ,resultado)
+print('El valor de la resta es ' ,resultado2)
 
 ###agregar un comentario
